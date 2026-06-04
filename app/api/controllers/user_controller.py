917f5561afb3353fe527082c6e27e66c4ca1df20
卢晓峰 <luxiaofeng.200@163.com>
2026-06-04 14:24:53 +0800

    1

--- app/api/controllers/user_controller.py
+++ app/api/controllers/user_controller.py
@@ -5,10 +5,13 @@
 职责：只负责路由定义、接收请求、参数校验、返回响应。
 严禁在此写任何业务逻辑——所有业务逻辑下沉到 Service 层。
 """
-
+import random
+import string
+import uuid
 from fastapi import APIRouter, Depends, Query, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.redis_client import redis_client
 from app.schemas.user import (
     ForgotPasswordRequest,
     ResetPasswordRequest,
@@ -20,6 +23,8 @@ from app.common.response import Response
 from app.core.dependency import get_current_user
 from app.database import get_session
 from app.services.user_service import UserService
+from io import BytesIO
+from captcha.image import ImageCaptcha
 
 # ==================== 路由定义 ====================
 router = APIRouter(prefix="/api/user", tags=["用户管理"])
@@ -158,3 +163,27 @@ async def delete_account(
         username=current_user["username"],
     )
     return Response.success(data=data, msg="账号已注销")
+
+# ===================获取验证码 ======================
+@router.get("/captcha",summary="获取验证码")
+async def captcha():
+    """生成图片验证码，返回 base64 图片和 captcha_id"""
+    code = "".join(random.choices(string.ascii_uppercase + string.digits, k=4))
+    # 生成一个唯一 ID
+    captcha_id = str(uuid.uuid4())
+
+    # 存 Redis 5分钟过期
+    await  redis_client.set(f"captcha_id:{captcha_id}", code,300)
+
+    # 生成图片
+    image = ImageCaptcha(width=160,height=80)
+
+    buf =BytesIO()
+    image.write(code , buf) #默认用png来进行填充
+    buf.seek(0)
+    import base64
+    returnRes = {
+        "captcha_id": captcha_id,
+        "image_base64": base64.b64encode(buf.getvalue()).decode(),
+    }
+    return Response.success(data=returnRes,msg ="获取验证码")
